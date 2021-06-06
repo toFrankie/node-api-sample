184047db266726595aa319f04e7f1add7fa28ebe
Frankie <1426203851@qq.com>
2021-06-07 00:37:25 +0800

    test: 调试

--- server-demo.js
+++ server-demo.js
@@ -4,39 +4,19 @@ const { getNetWorkIP } = require('./ipaddress')
 
 const localIPAddress = getNetWorkIP()
 const port = 7701
-const allowedOrigin = 'http://10.16.4.226:8080' // `http://${localIPAddress}:8080`
+const allowedOrigin = 'http://192.168.1.105:8080' // `http://${localIPAddress}:8080`
 
 const server = http.createServer((request, response) => {
-  console.log('')
-  console.log(`${request.method} ${request.url}`)
   if (request.headers.origin === allowedOrigin) {
     response.setHeader('Access-Control-Allow-Origin', allowedOrigin)
-    // response.setHeader('Access-Control-Allow-Credentials', true)
-    // response.setHeader('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With')
-    // response.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    response.setHeader('Access-Control-Allow-Credentials', true)
+    response.setHeader('Access-Control-Allow-Methods', 'PUT,OPTIONS')
+    response.setHeader('Access-Control-Allow-Headers', 'X-Custom-Header')
+    response.setHeader('Access-Control-Expose-Headers', 'Date,Access-Control-Allow-Origin')
   }
-  if (request.url == '/config') {
-    fs.readFile('./config.json', (err, data) => {
-      if (err) throw err
-      response.end(JSON.stringify({ name: 'Frankie', age: 20 }))
-    })
+  if (request.url === '/config') {
+    response.end(JSON.stringify({ name: 'Frankie', age: 20 }))
   }
-
-  // if (request.url === '/') {
-  //   fs.readFile('./index.html', (err, data) => {
-  //     if (err) throw err
-  //     response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' })
-  //     response.end(data)
-  //   })
-  // } else if (request.url == '/config') {
-  //   fs.readFile('./config.json', (err, data) => {
-  //     if (err) throw err
-  //     response.end(JSON.stringify({ name: 'Frankie', age: 20 }))
-  //   })
-  // }
-  // else {
-  //   console.log('其他请求', request.url)
-  // }
 })
 
 server.listen(port)
